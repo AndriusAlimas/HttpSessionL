236b204fe0416be703ea9a9d4a402b8abcbf6bc4
Andrius <andriusjavait@gmail.com>
2017-01-16 20:32:20 +0000

    update LoginServlet code, we can get parameter, create session object,set attribute in session scope

--- HttpSessionL/src/com/example/servlet/LoginServlet.java
+++ HttpSessionL/src/com/example/servlet/LoginServlet.java
@@ -6,6 +6,23 @@ import java.io.*;
 public class LoginServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request,
 			HttpServletResponse response)throws ServletException,IOException{
-		
+		// set content type with response:
+		response.setContentType("text/html");
+		// get PrintWriter using response object method getWriter() NOT getPrintWriter():
+        PrintWriter out = response.getWriter();
+        out.println("<html><title>HttpServletListener</title>");
+        // using request we get parameter from form(Query String):
+        String userName = request.getParameter("userName");
+        out.println("<body>Welcome <h4>" + userName + " </h4>");
+        
+        // HttpServletRequest interface has method to get session object:
+        HttpSession httpSession = request.getSession();
+        // invoking from session scope setAttribute from session:
+        httpSession.setAttribute("userName", userName);
+        
+        // simple hyperlink to another servlet:
+        out.println("<br><a href='logout.do'>logout</a>");
+        out.println("</body></html>");
+        out.close();
 	}
 }
