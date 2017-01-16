c0eb6f16e44671b28a6b973d55e9c435c5c272b5
Andrius <andriusjavait@gmail.com>
2017-01-16 20:38:06 +0000

    created template HttpSessionListener and added as well to DD

---
+++ HttpSessionL/src/com/example/listener/MyHttpSessionListener.java
@@ -0,0 +1,16 @@
+package com.example.listener;
+import javax.servlet.http.*;
+public class MyHttpSessionListener implements HttpSessionListener {
+	
+	@Override
+	public void sessionCreated(HttpSessionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sessionDestroyed(HttpSessionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+}
