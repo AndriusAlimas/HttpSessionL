bcdfa7f3259b76797b91ce51d97883c2686f6bed
Andrius <andriusjavait@gmail.com>
2017-01-17 19:38:48 +0000

    LogoutServlet coded to logout session invalidate

--- HttpSessionL/src/com/example/servlet/LogoutServlet.java
+++ HttpSessionL/src/com/example/servlet/LogoutServlet.java
@@ -6,6 +6,15 @@ import java.io.*;
 public class LogoutServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, 
 			HttpServletResponse response)throws ServletException,IOException{
-		
+			response.setContentType("text/html");
+	        PrintWriter out = response.getWriter();
+	        
+	        out.println("<html><title>HttpServletListener</title>");
+	        HttpSession httpSession = request.getSession(false);
+	        httpSession.invalidate();
+	        out.println("<body><h4>You are succesfully logout</h4>");
+	        out.println("</body></html>");
+	        
+	        out.close();
 	}
 }
